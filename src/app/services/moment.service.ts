abb5bd3f78613084ad3bee62863ea9cfff90141b
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-11-14 13:45:31 -0300

    feat: consumindo API

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Moment } from '../Moment';
+import { Response } from '../Response';
 import { environment } from 'src/environments/environments';
 
 @Injectable({
@@ -16,5 +17,9 @@ export class MomentService {
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData)
+  }
+
+  getMoments(): Observable<Response<Moment[]>>{
+    return this.http.get<Response<Moment[]>>(this.apiUrl)
   }
 }
